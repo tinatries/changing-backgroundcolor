6342532e9dae314e1b3f0fba1b726cecb8c74f52
tinatries <60852586+tinatries@users.noreply.github.com>
2022-04-15 23:07:24 -0400

    Add files via upload

---
+++ Changing_Background_Color/index.js
@@ -0,0 +1,9 @@
+let randNum =[0, 0, 0]; 
+
+
+
+btn.addEventListener("click", function(){
+    for (let i = 0; i < randNum.length; i++) randNum[i] = Math.floor(Math.random() * 257 - 1);
+    document.body.style.backgroundColor = 'rgb(' + randNum[0] + ',' + randNum[1] + ',' + randNum[2] + ')';
+    document.getElementsByClassName("rgb-display")[0].innerHTML = "RGB: " + randNum[0] + ', ' + randNum[1] + ', ' + randNum[2];
+})
