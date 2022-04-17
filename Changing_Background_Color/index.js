654c526fa4e63df9370e5aaccdec01d3c78f200d
tinatries <60852586+tinatries@users.noreply.github.com>
2022-04-17 16:12:45 -0400

    Update index.js

--- Changing_Background_Color/index.js
+++ Changing_Background_Color/index.js
@@ -3,7 +3,7 @@ let randNum =[0, 0, 0];
 
 
 btn.addEventListener("click", function(){
-    for (let i = 0; i < randNum.length; i++) randNum[i] = Math.floor(Math.random() * 257 - 1);
+    for (let i = 0; i < randNum.length; i++) randNum[i] = Math.floor(Math.random() * 256);
     document.body.style.backgroundColor = 'rgb(' + randNum[0] + ',' + randNum[1] + ',' + randNum[2] + ')';
     document.getElementsByClassName("rgb-display")[0].innerHTML = "RGB: " + randNum[0] + ', ' + randNum[1] + ', ' + randNum[2];
 })
